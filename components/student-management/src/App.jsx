09f9e498f5ba25e61570b94b236c28874c9b91b8
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-05-08 00:48:49 +0500

    working

--- components/student-management/src/App.jsx
+++ components/student-management/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='bg-blue-200 bg-[url("./assets/future-grid.jpg")] bg-cover bg-no-repeat bg-center min-h-screen w-full'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
---
+++ components/student-management/src/assets/assets.js
@@ -0,0 +1,11 @@
+import Logo from './logo.png';
+import Logo1 from './logo-1.png';
+import menu from './menu.svg';
+import close from './close.svg';
+
+export const assets = {
+    Logo,
+    Logo1,
+    menu,
+    close
+}
