c8d79abf5f9c427accac2dc3937023325d946ae4
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-05-25 12:50:00 +0500

    setting up project

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-red-300 text-3xl font-bold">Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
